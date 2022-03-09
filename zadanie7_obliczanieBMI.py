e52bb4f5a2b8dd475b1cc4c94270eac02f6e567c
KrystMroz <kryst.mroz96@gmail.com>
2022-03-09 13:05:30 +0100

    poprawienie błedu (wzrostw / wzrost w)

--- zadanie7_obliczanieBMI.py
+++ zadanie7_obliczanieBMI.py
@@ -1,4 +1,4 @@
-wzrost = float(input("Podaj swój wzrostw metrach: "))
+wzrost = float(input("Podaj swój wzrost w metrach: "))
 waga = int(input("Podaj swoją wagę: "))
 bmi = waga / wzrost**2
 print(f"Twoje BMI wynosi {bmi} ")
